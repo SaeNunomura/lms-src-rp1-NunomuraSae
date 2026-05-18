3d49a9f6a4f2d98aebf3e3011b2861b30714829a
edu <edu@192.168.100.26>
2026-05-18 14:21:01 +0900

    fix:布村 Task.25 不要な定数の削除
    StudentAttendanceService.java346行目を修正。使用する定数をローカル定数から、util/Constants.javaのstatic定数に変更。

--- trial_source/src/main/java/jp/co/sss/lms/service/StudentAttendanceService.java
+++ trial_source/src/main/java/jp/co/sss/lms/service/StudentAttendanceService.java
@@ -342,9 +342,8 @@ public class StudentAttendanceService {
 		//今日の日付を取得
 		Date trainingDate = attendanceUtil.getTrainingDate();
 		final int NO_DATA = 0;
-		final short DELETE_FLG = 0;
 		//[未入力日が0より大きい場合]:true,そうでない場合はfalseを戻す。
-		if (NO_DATA < tStudentAttendanceMapper.notEnterCount(loginUserDto.getLmsUserId(), (short) DELETE_FLG, trainingDate)) {
+		if (NO_DATA < tStudentAttendanceMapper.notEnterCount(loginUserDto.getLmsUserId(), Constants.DB_FLG_FALSE, trainingDate)) {
 			return true;
 		} else {
 			return false;
