70940bbbf615eada501849afe56d4dbdc4018d95
edu <edu@PC-013-NUNOMURA>
2026-06-08 14:43:47 +0900

    add: 布村 Task.58 ファイルの新規作成
    PlaceService.java 新規作成
    TPlaceIdUserAttendanceMapper.java 新規作成
    mapper/TPlaceIdUserAttendanceMapper.xml 新規作成

---
+++ trial_source/src/main/java/jp/co/sss/lms/form/BulkRegistForm.java
@@ -0,0 +1,8 @@
+package jp.co.sss.lms.form;
+
+import lombok.Data;
+
+@Data
+public class BulkRegistForm {
+
+}
---
+++ trial_source/src/main/java/jp/co/sss/lms/service/PlaceService.java
@@ -0,0 +1,9 @@
+package jp.co.sss.lms.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PlaceService {
+
+	
+}
